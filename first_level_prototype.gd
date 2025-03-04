8095e6005689e1e456b3905cf6cfe2e431c0fa3d
icebee03 <58120263+icebee03@users.noreply.github.com>
2025-03-04 20:10:11 +0100

    Progress On First Level

--- first_level_prototype.gd
+++ first_level_prototype.gd
@@ -1,17 +1,31 @@
 class_name FirstLevel extends Node
 
 @onready var cache :Cache = $Cache
-@onready var hitbox :CollisionShape2D = $Cache/Area2D/CollisionShape2D
+@onready var hitbox :CollisionShape2D = $Cache/Hitbox/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var midpointCache :Vector2 = cache.get_size() * 0.5
-	hitbox.set_position(midpointCache)
-	hitbox.shape.set_size(cache.get_size())
-	#print("Midpoint position: ",hitbox.get_position())
-	#print("hitbox size: ", hitbox.shape.get_size())
+	_fit_hitbox_to_cache()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# Fits the hitbox to the dynamic size of the cache
+func _fit_hitbox_to_cache() -> void:
+	var midpointOfCache :Vector2 = cache.get_size() * 0.5
+	hitbox.set_position(midpointOfCache)
+	hitbox.shape.set_size(cache.get_size())
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Area Entered!")
+	var label = area.get_parent()
+	if label is RichTextLabel:
+		var text = label.text
+		if text == "": return
+		print("Sorting "+text+" into cache")
+		cache.sort_address_into_cache(text)
---
+++ path_follow_2d.gd
@@ -0,0 +1,7 @@
+extends PathFollow2D
+
+func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($".", "progress_ratio", 1.0, 4)
+	await get_tree().create_timer(6.0).timeout
+	$"Floating Address".queue_free()
